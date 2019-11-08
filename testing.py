1321751abf4e72ec8414845a6e2c47e76a1ece69
Gabriel Orndorff <55101127+orndorffgabriel@users.noreply.github.com>
2019-11-08 16:52:51 -0600

    Update testing.py
    Finished up the unittest file. Ready for merge and testing.

--- testing.py
+++ testing.py
@@ -4,54 +4,58 @@
 #Import Statements
 import unittest
 import scipy as sp
-
-val = 23 #stand in value
+import optomize
 
 class Testing_foos(unittest.TestCase):
     '''
     This class is used in order to test the
-    functions that return the Overshoot and
-    Time to Settle values when given a zeta and
-    Wn.
+    functions that return the Zeta and
+    Wn values when given a percent overshoot
+    and time to settle.
     '''
     
-    def test_ts(self):
+    def test_zeta_wn(self):
         '''
-        Check to make sure that the time to 
-        settle is being calculated correctly.
+        Check to make sure that the Zeta
+        and Wn values are correct.
         '''
-        zeta  = -0.2
-        wn = -3
+        #Values for the test
+        os = 30
+        ts = 3
         
-        ts_val = 4/(zeta*wn)
-        self.assertAlmostEqual(val,ts_val,places = 14)
+        #Getting the correct values
+        test_z = (-sp.log(os/100))/sp.sqrt(sp.pi**2+sp.log(os/100)**2)
+        test_wn = 4/(ts*test_z)
         
-    def test_os(self):
-        '''
-        Check to make sure that the percent
-        overshoot is being calculated correctly.
-        '''
-        zeta  = -0.2
+        #Getting output from the file
+        z,wn = optomize.sys_vals(os,ts)
         
-        os_val = 100*sp.exp((-zeta*sp.pi)/sp.sqrt(1-zeta**2))
-        self.assertAlmostEqual(val,os_val,places = 14)
+        #Comparing the test to the actual
+        print(test_z, z, test_wn, wn)
+        self.assertAlmostEqual(z,test_z,places = 14)
+        self.assertAlmostEqual(wn,test_wn,places = 14)
+        
 
-class Testing_opto_ts(unittest.TestCase):
+class Testing_poles(unittest.TestCase):
     '''
-    This class is used to test the optimization of the
-    time to settle when given a percent overshoot to avoid.
+    This class is used to test the feedback gains that
+    should be outputted to the GUI
     '''
     
-    def test_zeta(self):
+    def test_K(self):
         '''
-        checking to make sure the zeta values are valid
+        Check the gain matrix
         '''
-        pass
-    
-    def test_wn(self):
-        '''
-        checking to make sure that the wn values are valid
-        '''
-        pass
+        
+        #Values for the test
+        os = 30
+        ts = 3
+        
+        #Actual value (calculated in MATLAB)
+        test_K = sp.array([9.57690, 0.6667])
+        K = optomize.for_the_gui(os,ts)
+        
+        print(test_K, K)
+        self.assertAlmostEqual(K,test_K, places = 14)
     
     pass
